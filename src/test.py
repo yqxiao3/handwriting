7410c752af0eb2cd4436951f9837644b0030bef6
XYQ_uV <xiao.yongqin@gmail.com>
2021-07-24 11:50:24 +0800

    add test.txt

--- src/test.py
+++ src/test.py
@@ -28,9 +28,10 @@ import network
 
 
 net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.SGD(training_data, 5, 10, 3.0, test_data=test_data)
 
-
+print('Finish')
+#quit()
 # ----------------------
 # - network2.py example:
 #import network2
@@ -124,33 +125,34 @@ net.SGD(training_data, 30, 10, 0.1,
 
 
 """
-#
-# def testTheano():
-#     from theano import function, config, shared, sandbox
-#     import theano.tensor as T
-#     import numpy
-#     import time
-#     print("Testing Theano library...")
-#     vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
-#     iters = 1000
-#
-#     rng = numpy.random.RandomState(22)
-#     x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
-#     f = function([], T.exp(x))
-#     print(f.maker.fgraph.toposort())
-#     t0 = time.time()
-#     for i in range(iters):
-#         r = f()
-#     t1 = time.time()
-#     print("Looping %d times took %f seconds" % (iters, t1 - t0))
-#     print("Result is %s" % (r,))
-#     if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
-#         print('Used the cpu')
-#     else:
-#         print('Used the gpu')
-# Perform check:
-#testTheano()
 
+# def testTheano():
+#      from theano import function, config, shared, sandbox
+#      import theano.tensor as T
+#      import numpy
+#      import time
+#      print("Testing Theano library...")
+#      vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
+#      iters = 1000
+#
+#      rng = numpy.random.RandomState(22)
+#      x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
+#      f = function([], T.exp(x))
+#      print(f.maker.fgraph.toposort())
+#      t0 = time.time()
+#      for i in range(iters):
+#          r = f()
+#      t1 = time.time()
+#      print("Looping %d times took %f seconds" % (iters, t1 - t0))
+#      print("Result is %s" % (r,))
+#      if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
+#          print('Used the cpu')
+#      else:
+#          print('Used the gpu')
+# # Perform check:
+# testTheano()
+#
+# exit()
 
 # ----------------------
 # - network3.py example:
@@ -163,12 +165,12 @@ training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
 
 # chapter 6 - shallow architecture using just a single hidden layer, containing 100 hidden neurons.
-'''
+
 net = Network([
     FullyConnectedLayer(n_in=784, n_out=100),
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
-'''
+net.SGD(training_data, 5, mini_batch_size, 0.1, validation_data, test_data)
+
 
 # chapter 6 - 5x5 local receptive fields, 20 feature maps, max-pooling layer 2x2
 '''
